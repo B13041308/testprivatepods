16f098d14c9907ca12f91f8a5a6368c5b92ed838
weikai <137142588@qq.com>
2018-04-08 15:51:08 +0800

    dkddkdkd

--- Classes/AppDelegate.h
+++ Classes/AppDelegate.h
@@ -6,12 +6,5 @@
 //  Copyright © 2018年 weikai. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
-
-@property (strong, nonatomic) UIWindow *window;
-
-
-@end
 
